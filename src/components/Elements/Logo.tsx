307dc65801b0508066aac971c0b000d03bb9ebca
Hidetoshi Hibino <hibinozzzz@gmail.com>
2023-08-03 19:01:26 +0900

    Logoの設定

--- src/components/Elements/Logo.tsx
+++ src/components/Elements/Logo.tsx
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <Link to="/">
-      <div className="logo">Logo</div>
-    </Link>
+    <div className="flex-shrink-0">
+      <Link to="/">
+        <img src={`${process.env.PUBLIC_URL}/Cocoaer_logo.jpeg`} alt="jpeg" className="h-10 w-30"/>
+      </Link>
+    </div>
    );
 }
 
