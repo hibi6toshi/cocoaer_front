fa652d8016c5154ec2eca8729012c2a46dc965c9
Hidetoshi Hibino <hibinozzzz@gmail.com>
2023-08-04 13:01:13 +0900

    logoの属性とテスト修正

--- src/components/Elements/Logo.tsx
+++ src/components/Elements/Logo.tsx
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <div className="flex-shrink-0">
       <Link to="/">
-        <img src={`${process.env.PUBLIC_URL}/Cocoaer_logo.jpeg`} alt="jpeg" className="h-10 w-30"/>
+        <img src={`${process.env.PUBLIC_URL}/Cocoaer_logo.jpeg`} alt="header_logo" className="h-10 w-30"/>
       </Link>
     </div>
    );
