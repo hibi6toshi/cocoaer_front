089c953cfa9d67bed2703b8717173dd76ec0f763
Hidetoshi Hibino <hibinozzzz@gmail.com>
2023-09-08 22:17:02 +0900

    Articleの検索機能の実装

--- src/pages/articles/Index.tsx
+++ src/pages/articles/Index.tsx
@@ -4,68 +4,60 @@ import ArticleCard from "../../features/articles/ArticleCard";
 import { Article, PaginationInfo } from "../../types";
 import Pagination from "../../components/Pagination/Pagination";
 import Button from "../../components/Elements/Button";
+import ArticleSearchModal from "../../components/Modals/ArticleSearchModal";
 
 export const loader: LoaderFunction = async ({ request } :LoaderFunctionArgs) => {
   const articles = await getArticles((new URL(request.url)));
-
-  return {articles: articles.data , pagination_info: articles.pagination_info};
+  return {articles: articles.data , pagination_info: articles.pagination_info, q: articles.q};
 }
 
 const IndexPage = () => {
   const { articles, pagination_info }  = useLoaderData() as {articles: Article[], pagination_info: PaginationInfo };
   const navigate = useNavigate();
 
-  if (articles.length === 0 ){
-    return (
+  return ( 
+    <>
       <div className="flex justify-center">
-        <div>
-          <div className="w-40">
+        <div className="w-80 flex flex-row justify-between">
+          <div className="w-60">
             <Button 
               label="新規作成"
               onClick={()=>navigate("/articles/new")}
               rounded_full
             />
           </div>
-          <div className="mt-32 font-bold text-lg">
-            "No Article found"
+          <ArticleSearchModal />
+        </div>
+      </div>
+      { articles.length === 0
+        ? 
+          <div className="flex justify-center mt-32 font-bold text-lg">
+            "No Coco found..."
           </div>
-        </div>
-      </div>
-    )
-  }
-
-  return ( 
-    <>
-      <div className="flex justify-center">
-        <div className="w-40">
-          <Button 
-            label="新規作成"
-            onClick={()=>navigate("/articles/new")}
-            rounded_full
-          />
-        </div>
-      </div>
-      <div className="
-        container 
-        mx-auto
-        grid
-        grid-cols-1
-        sm:grid-cols-2
-        ms:grid-cols-3
-        lg:grid-cols-3
-      ">
-        {articles?.map((article: Article) => (
-          <ArticleCard
-            key={article.id}
-            article={article}
-          />
-        ))}
-      </div>
-      <div className="mt-8">
-        <Pagination pagination_info={pagination_info}/>
-      </div>
+        :
+          <>
+            <div className="
+              container 
+              mx-auto
+              grid
+              grid-cols-1
+              sm:grid-cols-2
+              ms:grid-cols-3
+              lg:grid-cols-3
+            ">
+              {articles?.map((article: Article) => (
+                <ArticleCard
+                  key={article.id}
+                  article={article}
+                />
+              ))}
+            </div>
+            <div className="mt-8">
+              <Pagination pagination_info={pagination_info}/>
+            </div>
+          </>
+      }
     </>
-
    );
 }
 
