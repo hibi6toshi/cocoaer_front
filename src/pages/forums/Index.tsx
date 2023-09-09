729bf9898831fbf23c5142bc7b908b8c07d0d537
Hidetoshi Hibino <hibinozzzz@gmail.com>
2023-09-09 21:43:10 +0900

    forumの検索機能

--- src/pages/forums/Index.tsx
+++ src/pages/forums/Index.tsx
@@ -4,6 +4,7 @@ import ForumCard from "../../features/forums/ForumCard";
 import { Forum, PaginationInfo } from "../../types";
 import Pagination from "../../components/Pagination/Pagination";
 import Button from "../../components/Elements/Button";
+import ForumSearchModal from "../../components/Modals/ForumSearchModal";
 
 export const loader: LoaderFunction = async ({ request } :LoaderFunctionArgs) => {
   const forums = await getForums((new URL(request.url)));
@@ -37,32 +38,43 @@ const IndexPage = () => {
   return ( 
     <>
       <div className="flex justify-center">
-        <div className="w-40">
-          <Button 
-            label="新規作成"
-            onClick={()=>navigate("/forums/new")}
-            rounded_full
-          />
+        <div className="w-80 flex flex-row justify-between">
+          <div className="w-60">
+            <Button 
+              label="新規作成"
+              onClick={()=>navigate("/articles/new")}
+              rounded_full
+            />
+          </div>
+          <ForumSearchModal />
         </div>
       </div>
-    
-      <div className="
-        max-w-4xl
-        mx-auto
-        grid
-        grid-cols-1
-        m-4
-      "> 
-      {forums?.map((forum: Forum)=>(
-        <ForumCard
-          forum={forum}
-          key={forum.id}
-        />
-      ))}
-      </div>
-      <div className="mt-8">
-        <Pagination pagination_info={pagination_info}/>
-      </div>
+      { forums.length === 0
+        ?
+          <div className="flex justify-center mt-32 font-bold text-lg">
+            "No forum found..."
+          </div>
+        :
+          <>
+            <div className="
+              max-w-4xl
+              mx-auto
+              grid
+              grid-cols-1
+              m-4
+            "> 
+            {forums?.map((forum: Forum)=>(
+              <ForumCard
+                forum={forum}
+                key={forum.id}
+              />
+            ))}
+            </div>
+            <div className="mt-8">
+              <Pagination pagination_info={pagination_info}/>
+            </div>
+          </>
+      }
     </>
    );
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -193,3 +193,11 @@ export type ProjectQParams = {
   piety_target_id_in : string[];
   title_or_body_cont : string;
 }
+
+export type ForumQParams = {
+  cost_gteq : string;
+  cost_lteq : string;
+  piety_category_id_in : string[];
+  piety_target_id_in : string[];
+  title_or_body_cont : string;
+}
