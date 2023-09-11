9ceb74cdead8f381e5b822388ca75acdd8f26f0c
Hidetoshi Hibino <hibinozzzz@gmail.com>
2023-09-11 10:35:47 +0900

    navbarのスマホでのレイアウト崩れを解消

--- src/features/layouts/Navbar.tsx
+++ src/features/layouts/Navbar.tsx
@@ -1,30 +1,61 @@
 import ContentSelector from "./ContentSelector";
 import UserMenu from "./UserMenu";
 import Logo from "../../components/Elements/Logo";
+import useWindowSize from "../../hooks/useWindowSize";
 
 const Navbar = () => {
 
+  const [ width, height] = useWindowSize();
+
+  const body_if_width_large = (
+    <div
+      className="
+        flex
+        flex-row
+        items-center
+        justify-between
+        flex-wrap 
+        gap-3
+        md:gap-0
+      "
+    >
+      <Logo />
+      <ContentSelector />
+      <UserMenu />
+    </div>
+  );
+
+  const body_if_width_small = (
+    <>
+      <div
+        className="
+          flex
+          flex-row
+          items-center
+          justify-between
+          flex-wrap 
+          gap-3
+          md:gap-0
+        "
+      >
+        <Logo />
+        <UserMenu />
+      </div>
+      <div className="flex justify-center">
+          <ContentSelector />
+      </div>
+    </>
+  );
+
   return ( 
+    
     <div className="fixed w-full bg-white z-40 shadow-sm">
       <div className="
           p-2
           border-b-[1px]
         "
       >
-        <div
-          className="
-            flex
-            flex-row
-            items-center
-            justify-between
-            gap-3
-            md:gap-0
-          "
-        >
-          <Logo />
-          <ContentSelector />
-          <UserMenu />
-        </div>
+        { width > 550 ? body_if_width_large : body_if_width_small }
       </div>
     </div>
    );
--- src/pages/layouts/Layout.tsx
+++ src/pages/layouts/Layout.tsx
@@ -2,8 +2,10 @@ import { Outlet, ScrollRestoration, useNavigation } from "react-router-dom";
 import Navbar from "../../features/layouts/Navbar";
 import Footer from "../../features/layouts/Footer";
 import { Toaster } from "react-hot-toast";
+import useWindowSize from "../../hooks/useWindowSize";
 
 const Layout = () => {
+  const [ width, height ] = useWindowSize();
   const navigation = useNavigation();
   return ( 
     <div className="flex flex-col min-h-screen text-gray-700">
@@ -12,7 +14,7 @@ const Layout = () => {
       <div className={`
         flex-grow
         pb-10
-        pt-20
+        ${ width > 550 ? "pt-20" : "pt-24" }
         ${navigation.state === "idle" ? '' : 'opacity-50'}
       `}>
         <ScrollRestoration />
